0fcaf9a532f09812c68441cc7930bf0b3e524718
karyanayandi <xkaryanayandi@gmail.com>
2024-12-23 19:46:33 +0700

    fix(our-story): correct school name in story text
    Updated the school name from "Madrasah Aliyah Al-Ittifaq" to "MA Alif
    Al-Ittifaq" in the OurStory component. This change ensures the correct
    name is displayed in the story text.

--- src/components/our-story.tsx
+++ src/components/our-story.tsx
@@ -2,12 +2,12 @@ export function OurStory() {
   return (
     <div className="rounded-lg bg-white p-4 shadow">
       <p className="text-sm">
-        Yandi dan Shofa pertama kali bertemu di Madrasah Aliyah Al-Ittifaq pada
-        2014 dan menjalani hubugan jarak jauh pada 2015 karena Shofa pindah
-        sekolah. Pada 2021, Yandi mengunjungi rumah Shofa untuk menyampaikan
-        niat baiknya, tetapi rencana tersebut ditunda hingga Shofa menyelesaikan
-        kuliah. Insyaallah, mereka akan melangsungkan pernikahan pada 5 Januari
-        2025, berharap menjadi keluarga sakinah, mawaddah, dan warahmah.
+        Yandi dan Shofa pertama kali bertemu di MA Alif Al-Ittifaq pada 2014 dan
+        menjalani hubugan jarak jauh pada 2015 karena Shofa pindah sekolah. Pada
+        2021, Yandi mengunjungi rumah Shofa untuk menyampaikan niat baiknya,
+        tetapi rencana tersebut ditunda hingga Shofa menyelesaikan kuliah.
+        Insyaallah, mereka akan melangsungkan pernikahan pada 5 Januari 2025,
+        berharap menjadi keluarga sakinah, mawaddah, dan warahmah.
       </p>
     </div>
   )
