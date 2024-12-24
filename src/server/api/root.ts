1d62520bf831b214a0faa6ed864852007800a975
karyanayandi <xkaryanayandi@gmail.com>
2024-12-25 00:26:33 +0700

    feat(api): replace postRouter with messageRouter
    Updated the root API router to use messageRouter instead of postRouter.
    This change ensures that the API routes are correctly aligned with the
    new message handling functionality.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "@/server/api/routers/post"
+import { messageRouter } from "@/server/api/routers/message"
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc"
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  message: messageRouter,
 })
 
 // export type definition of API
