354c0e68168288c5b4701f00ae93a1f2970b6e3d
karyanayandi <xkaryanayandi@gmail.com>
2024-12-29 00:40:03 +0700

    feat(header): add links to avatar and title
    - Added Next.js Link component to wrap the Avatar and title elements
    - Ensured the avatar and title are clickable and navigate to the home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react"
+import Link from "next/link"
 import { Volume2, VolumeX } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -24,18 +25,22 @@ export function Header() {
   return (
     <header className="sticky top-0 z-10 flex items-center justify-between border-2 border-b-border bg-white p-2 text-[#54656f] lg:border-l-border lg:border-r-border">
       <div className="flex items-center space-x-2">
-        <Avatar className="h-10 w-10">
-          <AvatarImage
-            src="/us/couple.jpg"
-            alt="Couple"
-            className="aspect-square object-cover"
-          />
-          <AvatarFallback className="text-[#54656f]">Y&S</AvatarFallback>
-        </Avatar>
+        <Link href="/">
+          <Avatar className="h-10 w-10">
+            <AvatarImage
+              src="/us/couple.jpg"
+              alt="Couple"
+              className="aspect-square object-cover"
+            />
+            <AvatarFallback className="text-[#54656f]">Y&S</AvatarFallback>
+          </Avatar>
+        </Link>
         <div>
-          <h1 className="text-base font-semibold text-black lg:text-lg">
-            Jadi Nikah
-          </h1>
+          <Link href="/">
+            <h1 className="text-base font-semibold text-black lg:text-lg">
+              Jadi Nikah
+            </h1>
+          </Link>
           <p className="text-xs opacity-80">Minggu, 5 Januari, 2025</p>
         </div>
       </div>
